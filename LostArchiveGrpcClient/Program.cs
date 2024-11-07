d4db463b8d66eaa6a521f65b57290efc8dee34aa
Mike <m.a.ilnitsky@yandex.ru>
2024-11-07 16:56:49 +0700

    Created test clients for archive and wallet

---
+++ LostArchiveGrpcClient/Program.cs
@@ -0,0 +1,74 @@
+﻿using Grpc.Net.Client;
+using LotsArchiveGrpcClient;
+using System;
+using System.Threading.Tasks;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("https://localhost:7100");
+
+// создаем клиент
+var client = new Copying.CopyingClient(channel);
+
+(string, string)[] requests =
+[
+    ("", ""),
+    ("123", "123"),
+    ("", "22222222-5717-4562-b3fc-222200001111"),
+    ("11111111-5717-4562-b3fc-111100001111", ""),
+    ("11111111-5717-4562-b3fc-999900001111", "22222222-5717-4562-b3fc-999900001111"),
+    ("11111111-5717-4562-b3fc-111100001111", "22222222-5717-4562-b3fc-222200001111"),
+    ("11111111-5717-4562-b3fc-111100001111", "22222222-5717-4562-b3fc-222200005555"),
+    ("11111111-5717-4562-b3fc-111100004444", "22222222-5717-4562-b3fc-22220000aaaa"),
+];
+
+foreach (var item in requests)
+{
+    await TestLotGetting(client, item.Item1, item.Item2);
+}
+
+async Task TestLotGetting(Copying.CopyingClient client, string sellerId, string lotId)
+{
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Request:");
+    Console.WriteLine($"SellerId = {sellerId}");
+    Console.WriteLine($"LotId = {lotId}");
+    Console.WriteLine();
+
+    var response = await GetLotCopy(client, sellerId, lotId);
+
+    Console.WriteLine("Response:");
+
+    if (response.IsError)
+    {
+        Console.WriteLine($"ERROR: {response.ErrorMessage}");
+    }
+    else
+    {
+        PrintLotInfo(response.LotCopy);
+    }
+
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine();
+}
+
+async Task<GetLotCopyResponseGrpc> GetLotCopy(Copying.CopyingClient client, string sellerGuid, string lotGuid)
+{
+    var request = new GetLotCopyQueryGrpc
+    {
+        SellerId = sellerGuid,
+        LotId = lotGuid
+    };
+
+    return await client.GetLotCopyAsync(request);
+}
+
+void PrintLotInfo(LotCopyModelGrpc lot)
+{
+    Console.WriteLine($"Id = {lot.LotInfo.Id}");
+    Console.WriteLine($"Title = {lot.LotInfo.Title}");
+    Console.WriteLine($"Description = {lot.LotInfo.Description}");
+    Console.WriteLine($"StartingPrice = {lot.LotParams.StartingPrice}");
+    Console.WriteLine($"BidIncrement = {lot.LotParams.BidIncrement}");
+    Console.WriteLine($"RepurchasePrice = {lot.LotParams.RepurchasePrice?.ToString() ?? "null"}");
+}
---
+++ WalletGrpcClient/Program.cs
@@ -0,0 +1,137 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using WalletGrpcClient;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("https://localhost:7142");
+
+// создаем клиент
+var client = new Traiding.TraidingClient(channel);
+
+// Здесь должны быть ошибки:
+await ReserveMoney("", 0, "", "", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 0, "", "", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", -100, "", "", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 100, "", "", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 100, "22222222-5717-4562-b3fc-777700001111", "", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 100, "22222222-5717-4562-b3fc-777700001111", "Лот №1", "");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001119", 100, "22222222-5717-4562-b3fc-777700001111", "Лот №1", "Описание лота №1");
+
+// Здесь не должно быть ошибок, если в кошельке достаточно денег:
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 100, "22222222-5717-4562-b3fc-777700001111", "Лот №1", "Описание лота №1");
+await RealeaseMoney("11111111-5717-4562-b3fc-111100001111", 100, "22222222-5717-4562-b3fc-777700001111");
+await ReserveMoney("11111111-5717-4562-b3fc-111100002222", 200, "22222222-5717-4562-b3fc-777700001111", "Лот №1", "Описание лота №1");
+await RealeaseMoney("11111111-5717-4562-b3fc-111100002222", 200, "22222222-5717-4562-b3fc-777700001111");
+await ReserveMoney("11111111-5717-4562-b3fc-111100001111", 500, "22222222-5717-4562-b3fc-777700001111", "Лот №1", "Описание лота №1");
+await PayForLot("11111111-5717-4562-b3fc-111100001111", 500, "22222222-5717-4562-b3fc-777700001111", "11111111-5717-4562-b3fc-111100003333");
+
+async Task PayForLot(string buyerId, double hammerPrice, string lotId, string sellerId)
+{
+    var request = new PayForLotCommandGrpc
+    {
+        BuyerId = buyerId,
+        SellerId = sellerId,
+        LotId = lotId,
+        HammerPrice = hammerPrice
+    };
+
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Request:");
+    PrintPayForLotCommand(request);
+    Console.WriteLine();
+
+    var response = await client.PayForLotAsync(request);
+
+    Console.WriteLine("Response:");
+    PrintResponse(response);
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine();
+}
+
+async Task RealeaseMoney(string buyerId, double price, string lotId)
+{
+    var request = new RealeaseMoneyCommandGrpc
+    {
+        BuyerId = buyerId,
+        LotId = lotId,
+        Price = price
+    };
+
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Request:");
+    PrintRealeaseMoneyCommand(request);
+    Console.WriteLine();
+
+    var response = await client.RealeaseMoneyAsync(request);
+
+    Console.WriteLine("Response:");
+    PrintResponse(response);
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine();
+}
+
+async Task ReserveMoney(string buyerId, double price, string lotId, string lotTitle, string lotDescription)
+{
+    var request = new ReserveMoneyCommandGrpc
+    {
+        BuyerId = buyerId,
+        Price = price,
+        Lot = new LotInfoModelGrpc
+        {
+            Id = lotId,
+            Title = lotTitle,
+            Description = lotDescription
+        }
+    };
+
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Request:");
+    PrintReserveMoneyCommand(request);
+    Console.WriteLine();
+
+    var response = await client.ReserveMoneyAsync(request);
+
+    Console.WriteLine("Response:");
+    PrintResponse(response);
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine();
+}
+
+
+void PrintPayForLotCommand(PayForLotCommandGrpc request)
+{
+    Console.WriteLine($"BuyerId = {request.BuyerId}");
+    Console.WriteLine($"SellerId = {request.SellerId}");
+    Console.WriteLine($"LotId = {request.LotId}");
+    Console.WriteLine($"HammerPrice = {request.HammerPrice}");
+}
+
+void PrintRealeaseMoneyCommand(RealeaseMoneyCommandGrpc request)
+{
+    Console.WriteLine($"BuyerId = {request.BuyerId}");
+    Console.WriteLine($"LotId = {request.LotId}");
+    Console.WriteLine($"Price = {request.Price}");
+}
+
+void PrintReserveMoneyCommand(ReserveMoneyCommandGrpc request)
+{
+    Console.WriteLine($"BuyerId = {request.BuyerId}");
+    Console.WriteLine($"Price = {request.Price}");
+    Console.WriteLine($"Lot.Id = {request.Lot.Id}");
+    Console.WriteLine($"Lot.Title = {request.Lot.Title}");
+    Console.WriteLine($"Lot.Description = {request.Lot.Description}");
+}
+
+void PrintResponse(BaseResponseGrpc response)
+{
+    if (response.IsError)
+    {
+        Console.WriteLine($"ERROR: {response.Message}");
+    }
+    else
+    {
+        Console.WriteLine($"OK: {response.Message}");
+    }
+}

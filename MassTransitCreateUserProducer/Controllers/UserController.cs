7971d25024d4fbcaf0beca0e1424c032a6c9a439
Mike <m.a.ilnitsky@yandex.ru>
2024-11-12 11:44:18 +0700

    Added MassTransitCreateUserProducer

---
+++ MassTransitCreateUserProducer/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Otus.QueueDto.User;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MassTransitCreateUserProducer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IPublishEndpoint publishEndpoint) : ControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateUserEvent request)
+    {
+        await publishEndpoint.Publish(request);
+        return Ok();
+    }
+}
